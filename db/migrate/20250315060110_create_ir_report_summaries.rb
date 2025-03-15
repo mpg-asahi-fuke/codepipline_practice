f214f3ad35c05b909f21a0d6b5a2944a26061d53
mpg-asahi-fuke <115079196+mpg-asahi-fuke@users.noreply.github.com>
2025-03-15 15:05:39 +0900

    IrReport::Summaryを作成する

---
+++ db/migrate/20250315060110_create_ir_report_summaries.rb
@@ -0,0 +1,10 @@
+class CreateIrReportSummaries < ActiveRecord::Migration[8.0]
+  def change
+    create_table :ir_report_summaries do |t|
+      t.references :ir_report, null: false, foreign_key: true
+      t.text       :content,   null: false
+      t.integer    :type,      null: false
+      t.timestamps
+    end
+  end
+end
