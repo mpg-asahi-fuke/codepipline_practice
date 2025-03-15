4ce74acf490e1b8ae8e5ece57060bdb9061a28e1
mpg-asahi-fuke <115079196+mpg-asahi-fuke@users.noreply.github.com>
2025-03-15 14:57:16 +0900

    ir_reportsのモデルを作成する

---
+++ app/models/ir_report.rb
@@ -0,0 +1,2 @@
+class IrReport < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_15_053721) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_15_055132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,4 +20,17 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_15_053721) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "ir_reports", force: :cascade do |t|
+    t.bigint "company_id", null: false
+    t.date "published_at", null: false
+    t.string "title", null: false
+    t.text "content"
+    t.text "url", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_ir_reports_on_company_id"
+  end
+
+  add_foreign_key "ir_reports", "companies"
 end
